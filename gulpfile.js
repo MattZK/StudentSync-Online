dadeb147ec5ee6d96242b669acbb7ffb8f425264
Matthias Willemsen <mattwillemsen@icloud.com>
2018-02-20 18:16:09 +0100

    Fixed up code

--- gulpfile.js
+++ gulpfile.js
@@ -25,22 +25,22 @@ gulp.task('serve', ['default'], () => {
   gulp.watch('src/*.html', () => {gulp.src('*.html').pipe(gulp.dest('dist')).on('change', browserSync.reload)});
   gulp.watch('src/scss/*.scss', () => {gulp.src('scss/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./maps')).pipe(gulp.dest('dist/css')).pipe(browserSync.stream())});
   gulp.watch('src/img/*', () => {gulp.src('img/*').pipe(gulp.dest('dist/img'))});
-  gulp.watch('src/js/*.js', () => {gulp.src('js/*.js').pipe(concat('main.js')).pipe(babel({presets: ['env']})).pipe(gulp.dest('dist/js')).on('change', browserSync.reload)});
-  gulp.watch('src/ts/*.ts', () => {gulp.src('ts/*.ts').pipe(concat('main.ts')).pipe(ts({noImplicitAny: true})).pipe(babel({presets: ['env']})).pipe(gulp.dest('dist/js')).on('change', browserSync.reload)});
+  gulp.watch('src/js/*.js', () => {gulp.src('js/*.js').pipe(babel({presets: ['env']})).pipe(gulp.dest('dist/js')).on('change', browserSync.reload)});
+  gulp.watch('src/ts/*.ts', () => {gulp.src('ts/*.ts').pipe(ts({noImplicitAny: true})).pipe(babel({presets: ['env']})).pipe(gulp.dest('dist/js')).on('change', browserSync.reload)});
 });
 
 gulp.task('default', () => {
   gulp.src('src/*.html').pipe(gulp.dest('dist'));
   gulp.src('src/scss/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./maps')).pipe(gulp.dest('dist/css'));
   gulp.src('src/img/*').pipe(gulp.dest('dist/img'));
-  gulp.src('src/js/*.js').pipe(concat('main.js')).pipe(babel({presets: ['env']})).pipe(gulp.dest('dist/js'));
-  gulp.src('src/ts/*.ts').pipe(concat('main.ts')).pipe(ts({noImplicitAny: true})).pipe(babel({presets: ['env']})).pipe(gulp.dest('dist/js'));
+  gulp.src('src/js/*.js').pipe(babel({presets: ['env']})).pipe(gulp.dest('dist/js'));
+  gulp.src('src/ts/*.ts').pipe(ts({noImplicitAny: true})).pipe(babel({presets: ['env']})).pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('build', () => {
   gulp.src('src/*.html').pipe(gulp.dest('build'));
   gulp.src('src/scss/*.scss').pipe(sass()).pipe(gulp.dest('build/css'));
   gulp.src('src/img/*').pipe(imagemin()).pipe(gulp.dest('build/img'));
-  gulp.src('src/js/*.js').pipe(concat('main.js')).pipe(babel({presets: ['env']})).pipe(gulp.dest('build/js'));
-  gulp.src('src/ts/*.ts').pipe(concat('main.ts')).pipe(ts({noImplicitAny: true})).pipe(babel({presets: ['env']})).pipe(gulp.dest('build/js'));
+  gulp.src('src/js/*.js').pipe(babel({presets: ['env']})).pipe(gulp.dest('build/js'));
+  gulp.src('src/ts/*.ts').pipe(ts({noImplicitAny: true})).pipe(babel({presets: ['env']})).pipe(gulp.dest('build/js'));
 });
